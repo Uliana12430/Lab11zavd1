5b7386cdb0f3dc9655782e85f2b21a168b344162
ulian <ulian@JULY>
2024-11-18 00:20:40 +0200

    Добавьте файлы проекта.

---
+++ Lab11zavd1/Lab11zavd1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n = 12; 
+    int a0 = 1, a1 = 2;
+    int a2;
+
+    cout << "Перші " << n << " елементів послідовності:" << endl;
+
+    cout << a0 << " " << a1 << " "; 
+
+    for (int i = 2; i < n; i++) {
+        a2 = 3 * a1 + 2 * a0;
+        cout << a2 << " ";
+        a0 = a1;
+        a1 = a2;
+    }
+
+    cout << endl;
+
+    return 0;
+}
